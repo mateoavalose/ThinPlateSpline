06552bea564d362e69ec52947e97df9d41cbaa8a
Mateo <mateo.avalos3942@gmail.com>
2024-05-12 18:00:24 -0500

    Added functions for derivates

--- Files/TPS.py
+++ Files/TPS.py
@@ -12,12 +12,31 @@ class TPS:
         # In the mean time, just use the x values as the centres
         Centres = self.x_values
         return Centres
-
+    
+    def calcr(self, xc, x):
+        return np.sqrt(np.power((x - xc), 2)) 
+    
     def calcTPS(self, xc, x):
-            if(xc == x):
-                return 0
-            r = np.sqrt(np.power((xc - x), 2))
-            return self.b * np.power(r, 2*self.a) * np.log(self.b * r)
+        if(xc == x):
+            return 0
+        r = self.calcr(xc, x)
+        return self.b * np.power(r, 2*self.a) * np.log(self.b * r)
+    
+    def DevcalcTPS(self, xc, x):
+        if(xc == x):
+            return 0
+        r = self.calcr(xc, x)
+        dIdr = self.b * np.power(r, 2*self.a-1) * (2*self.a*np.log(self.b*r)+1)
+        return dIdr * (x - xc) / r
+    
+    def Dev2calcTPS(self, xc, x):
+        if(xc == x):
+            return 0
+        r = self.calcr(xc, x)
+        dIdr = self.b * np.power(r, 2*self.a-1) * (2*self.a*np.log(self.b*r)+1)
+        d2Idr2 = self.b*np.power(r, 2*self.a-2)*(2*self.a*(2*self.a-1)*np.log(self.b*r)+4*self.a-1)
+        return d2Idr2*np.power((x-xc)/r, 2) + dIdr*(1/r-(np.power((x-xc), 2)/np.power(r, 3)))
+
     def matrix_A(self):
         A = np.empty((len(self.x_values), len(self.Centres)))
         for i in range(len(self.x_values)):
@@ -30,4 +49,18 @@ class TPS:
         result = 0.0
         for i in range(n):
             result += w[i] * self.calcTPS(self.Centres[i], x)
+        return result
+    
+    def interpolateDerivate(self, w, x):
+        n = len(self.Centres)
+        result = 0.0
+        for i in range(n):
+            result += w[i] * self.DevcalcTPS(self.Centres[i], x)
+        return result
+    
+    def interpolateDerivate2(self, w, x):
+        n = len(self.Centres)
+        result = 0.0
+        for i in range(n):
+            result += w[i] * self.Dev2calcTPS(self.Centres[i], x)
         return result
